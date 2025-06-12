2b33777c3823ae857a96f7cb6e2fa90c5e38d924
naderr444 <anhhby09@gmail.com>
2025-06-12 22:27:27 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحبا بك في FastFlex 🚀</h1>
+      <h1>مرحباً بك في FastFlex 🚀</h1>
+    </div>
+  );
+}
